9f087be8c9584c71deef2f04bba012ad18d5c310
mellanie-k <mellaniekosgey@gmail.com>
2026-09-16 23:34:31 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,19 @@
+/*Author:Mellanie Kosgei
+Reg No:BCS-05-0543/2026
+Description:Units offered 
+Version 1
+*/
+
+#include<stdio.h>
+
+int main(){
+	//units offered this semester
+	printf("Introduction to comp studies \n");
+	printf("Communication skills \n");
+	printf("Computational logic \n");
+	printf("Basic maths \n");
+	printf("structure and programming \n");
+	printf("introduction to physics \n");
+	printf("health and wellness awareness \n");
+	return 0;
+}
